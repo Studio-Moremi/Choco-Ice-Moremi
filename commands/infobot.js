43cfe3c3b18c96047b9782c097b6ab6bcfc89857
cookedas1 <mulmando1991@gmail.com>
2024-11-09 19:24:22 +0900

    [ALPHA] 241109: infobot.js update

--- commands/infobot.js
+++ commands/infobot.js
@@ -13,7 +13,7 @@ module.exports = {
       .setDescription('초코 아이스 모레미에 대한 정보에요.')
       .addFields(
         { name: 'Name', value: 'Choco Ice Moremi', inline: true },
-        { name: 'version', value: 'alpha 1.0.0', inline: true },
+        { name: 'version', value: 'alpha 1.0.1', inline: true },
         { name: 'Package', value: 'Package list', inline: true },
         { name: 'Package2', value: 'discord.js - 14.16.3', inline: true },
         { name: 'Package3', value: 'mongoose - 8.8.0', inline: true },
