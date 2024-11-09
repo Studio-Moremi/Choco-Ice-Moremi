d9d171934bdf133594f101040a5425225925a0dc
cookedas1 <mulmando1991@gmail.com>
2024-11-09 21:08:20 +0900

    [ALPHA] 241109: lauguage add

--- commands/infobot.js
+++ commands/infobot.js
@@ -17,10 +17,10 @@ module.exports = {
       .setDescription('초코 아이스 모레미에 대한 정보에요.')
       .addFields(
         { name: 'Name', value: 'Choco Ice Moremi', inline: true },
+        { name: 'lauguage', value: 'node.js, sqlite3, mysql' inline: true },
         { name: 'version', value: 'alpha 1.0.1', inline: true },
         { name: 'Package', value: 'Package list', inline: true },
         { name: 'Package2', value: 'discord.js - 14.16.3', inline: true },
-        { name: 'Package3', value: 'mongoose - 8.8.0', inline: true },
         { name: 'Package4', value: 'dotenv - 16.4.5', inline: true },
         { name: 'Package5', value: 'axios - 1.7.7', inline: true },
         { name: 'Package6', value: 'cheerio - 1.0.0', inline: true },
