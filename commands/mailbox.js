abd9f88c8b8d9ce2d1cea1ef4fd44b2c20e51812
cookedas1 <mulmando1991@gmail.com>
2024-11-09 21:27:38 +0900

    [ALPHA] 241109: /우편함 명령어 추가

---
+++ commands/mailbox.js
@@ -0,0 +1,26 @@
+/* License is GPL 3.0.
+- made by studio moremi
+ - support@studio-moremi.kro.kr
+*/
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: {
+    name: '우편함',
+    description: '📬 우편함에 든 편지를 확인해요.'
+  },
+
+  run: async ({ interaction }) => {
+    const embed = new EmbedBuilder()
+      .setColor(0xffffff)
+      .setTitle('📬 우편함')
+      .setDescription('편지가 왔어요!')
+      .addFields(
+        { name: '[alpha 1.0.1] 업데이트', value: '- /가입 명령어 버그 수정/n- /농장 명령어 추가\n- /공동농장 명령어 추가', inline: true },
+        { name: '[alpha 1.0.0] 업데이트', value: '- /가입 명령어 추가\n- /인벤토리 명령어 추가\n- /봇정보 명령어 추가\n- /도움말 명령어 추가', inline: true }
+      )
+      .setFooter({ text: '오늘도 초코 아이스 모레미와 놀아보세요!' });
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
