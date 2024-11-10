639a84da7c92414e6de83112609df072f1530b0d
cookedas1 <mulmando1991@gmail.com>
2024-11-10 13:17:57 +0900

    [ALPHA] 241110: modify channelfarm.js

--- commands/channelfarm.js
+++ commands/channelfarm.js
@@ -24,16 +24,16 @@ module.exports = {
     db.get(`SELECT * FROM users WHERE discord_id = ?`, [discordId], (err, userRow) => {
       if (err) {
         console.error('Database error:', err.message);
-        return interaction.reply({ content: '에러가 발생했어요! 관리자에게 문의하세요.', ephemeral: true });
+        return interaction.reply({ content: `error100`, ephemeral: true });
       }
       if (!userRow) {
-        return interaction.reply({ content: '계정에 로그인되지 않았어요. /가입을 통해 로그인해봐요!', ephemeral: true });
+        return interaction.reply({ content: `error104`, ephemeral: true });
       }
 
       db.get(`SELECT farm_data FROM shared_farm WHERE channel_id = ?`, [channel.id], async (err, farmRow) => {
         if (err) {
           console.error('Database error:', err.message);
-          return interaction.reply({ content: '에러가 발생했어요! 관리자에게 문의하세요.', ephemeral: true });
+          return interaction.reply({ content: `error100`, ephemeral: true });
         }
 
         const farmData = farmRow ? JSON.parse(farmRow.farm_data) : initialSharedFarm;
@@ -47,14 +47,14 @@ module.exports = {
           .addComponents(
             new StringSelectMenuBuilder()
               .setCustomId('shared_farm_action')
-              .setPlaceholder('아이템 선택')
+              .setPlaceholder(`SelectItem`)
               .addOptions([
-                { label: '상추 심기', description: '상추를 심어요.', value: 'plant_lettuce' },
-                { label: '토마토 심기', description: '토마토를 심어요.', value: 'plant_tomato' },
-                { label: '딸기 심기', description: '딸기를 심어요.', value: 'plant_strawberry' },
-                { label: '물주기', description: '모든 작물에 물을 줘요.', value: 'water_plants' },
-                { label: '썩은 식물 치우기', description: '썩은 작물을 치워요.', value: 'clear_withered' },
-                { label: '수확하기', description: '모든 작물을 수확해요.', value: 'harvest' }
+                { label: `plantLettuce`, description: `plantLettuce1`, value: 'plant_lettuce' },
+                { label: `plantTomato`, description: `plantTomato1`, value: 'plant_tomato' },
+                { label: `plantStrawberry`, description: `plantStrawberry1`, value: 'plant_strawberry' },
+                { label: `waterPlants`, description: `waterPlants1`, value: 'water_plants' },
+                { label: `clearWithered`, description: `clearWithered1`, value: 'clear_withered' },
+                { label: `harvest`, description: `harvest1`, value: 'harvest' }
               ])
           );
 
