5f49a6ff00224a0aebcbfa50957ebeed0ceb65a7
cookedas1 <mulmando1991@gmail.com>
2024-11-10 16:20:04 +0900

    [ALPHA] 241110: add install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+npm install mongoose@8.8.0
+npm install discord.js@14.16.3
+npm install dotenv@16.4.5
+npm install axios@1.7.7
+npm install cheerio@1.0.0
+npm install sqlite3@5.1.7
+npm install djs-commander@0.0.50
