6256dc23b1cf7d4c8b8dc042b84293632de3c7b4
cookedas1 <mulmando1991@gmail.com>
2024-11-20 16:51:40 +0900

    Create stream.js

---
+++ commands/stream/stream.js
@@ -0,0 +1,34 @@
+/* License is GPL 3.0.
+- made by Studio moremi.
+ - support@studio-moremi.kro.kr
+*/
+// 아 djs commander 안넣었누
+const createResource = require('./resource.js');
+const createConnection = require('./connection.js');
+const queue = require('../index.js');
+
+module.export = function stream(interaction,url, run) {
+ let connection;
+ let resource;
+ let player;
+ 
+ 
+ if(queue.length == 0) {
+  connection = createConnection(interaction);
+  player = createAudioPlayer({
+  behaviors: {
+   noSubscriber: NoSubscriberBehavior.Stop,
+  },
+ })
+ };
+ 
+ player = queue[0].player;
+ connection = queue[0].connection;
+ resource = createResource(url);
+ 
+ player.play(resource);
+ 
+ connection.subscribe(player);
+ 
+ run(player, connection);
+}
